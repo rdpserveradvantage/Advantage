d36487ad4be3751828a8208534396cc29eaee282
Aniruddh <rdpthisserver@gmail.com>
2023-09-30 20:35:33 +0530

    added db backup

--- clarity/faultyMaterial.php
+++ clarity/faultyMaterial.php
@@ -10,45 +10,64 @@
                         <div class="card-block">
 
                             <?
-                            echo $statement = "select * from generatefaultymaterialrequest where requestBy='" . $PROJECT_email . "' and requestByPortal in ('Clarity','Clarify')" ; 
+                            $statement = "SELECT * FROM generatefaultymaterialrequest WHERE requestBy='" . $PROJECT_email . "' AND requestByPortal IN ('Clarity','Clarify') and materialRequestLevel=3 and status=1";
                             $sql = mysqli_query($con, $statement);
+
                             if (mysqli_num_rows($sql) > 0) {
                                 echo '
-                                <table class="table table-bordered table-striped table-hover dataTable js-exportable no-footer table-xs">
-                                <thead>
-                                <tr class="table-primary">
-                                <th>Sr No</th>
-                                <th>Material</th>
-                                <th>Serial Number</th>
-                                <th>ATMID</th>
-                                <th>Action</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                
+    <table class="table table-bordered table-striped table-hover dataTable js-exportable no-footer table-xs">
+    <thead>
+    <tr class="table-primary">
+    <th>Sr No</th>
+    <th>Material</th>
+    <th>Serial Number</th>
+    <th>ATMID</th>
+    <th>Action</th>
+    </tr>
+    </thead>
+    <tbody>';
 
-                                ';
                                 $i = 1;
+
                                 while ($sql_result = mysqli_fetch_assoc($sql)) {
-                                    $MaterialName = $sql_result['MaterialName'];
-                                    $MaterialSerialNumber = $sql_result['MaterialSerialNumber'];
+                                    echo $id = $sql_result['id'];
                                     $atmid = $sql_result['atmid'];
 
                                     echo "<tr>
-                                    <td>$i &nbsp;&nbsp;<input type='checkbox' name='materialId[]'</td>
-                                    <td>$MaterialName</td>
-                                    <td>$MaterialSerialNumber</td>
-                                    <td>$atmid</td>
-                                    <td><a href='#'>Dispatch Item </a></td>
-                                    </tr>";
+                <td>$i &nbsp;&nbsp;&nbsp;
+                 <input type='checkbox' name='materialRequestId[]' value='$id' /> 
+                 </td>
+                <td class='strong' colspan='3'>$atmid</td>
+                <td><a href='#'>Dispatch Item</a></td>
+
+              </tr>";
+
+                                    $detailsSql = mysqli_query($con, "SELECT * FROM generatefaultymaterialrequestdetails WHERE requestId='" . $id . "'");
+                                    $counter2 = 1;
+
+                                    while ($detailsSql_result = mysqli_fetch_assoc($detailsSql)) {
+                                        $MaterialName = $detailsSql_result['MaterialName'];
+                                        $MaterialSerialNumber = $detailsSql_result['MaterialSerialNumber'];
+
+                                        echo "<tr>
+                    <td></td>
+                    <td>$MaterialName</td>
+                    <td>$MaterialSerialNumber</td>
+                    <td></td>
+                    <td></td>
+                  </tr>";
+
+                                        $counter2++;
+                                    }
+
                                     $i++;
                                 }
-                                echo '</tbody>
-                                </table>';
-                                echo '<a href="#" class="btn btn-primary">Dispatch Checked Item </a>';
 
+                                echo '</tbody>
+    </table>';
+                                echo '<a href="#" class="btn btn-primary" onclick="dispatchCheckedItems()">Dispatch Checked Item</a>                                ';
                             } else {
-                                echo 'No Data Found ! ';
+                                echo 'No Data Found!';
                             }
 
                             ?>
@@ -62,5 +81,30 @@
     </div>
 </div>
 
+<script>
+function dispatchCheckedItems() {
+    var materialRequestIds = document.getElementsByName('materialRequestId[]');
+    var selectedItems = [];
+
+    for (var i = 0; i < materialRequestIds.length; i++) {
+        if (materialRequestIds[i].checked) {
+            selectedItems.push(materialRequestIds[i].value);
+        }
+    }
+
+    // Send the selected items to the server using AJAX
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            // Display the response from the server
+            console.log(xhr.responseText);
+        }
+    };
+
+    xhr.open('POST', 'process_selected_items.php', true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send('selectedItems=' + JSON.stringify(selectedItems));
+}
+</script>
 
 <? include('footer.php'); ?>

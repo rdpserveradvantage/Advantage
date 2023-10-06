3713b5061c9c8ce30e73435a19ec3195af3d1456
Aniruddh <rdpthisserver@gmail.com>
2023-10-06 20:06:03 +0530

    auto backup (2023-10-06)

--- backup.php
+++ backup.php
@@ -1,38 +1,73 @@
 <?php
-
-$host = '10.63.21.6';
+$host = 'localhost';
 $username = 'advantage';
 $password = 'qwerty121';
 $database = 'sarmicrosystems_advantage';
 
-$mysqli = new mysqli($host, $username, $password, $database);
 
-if ($mysqli->connect_error) {
-    die("Connection failed: " . $mysqli->connect_error);
+$backupFolder = 'database_backup';
+if (!is_dir($backupFolder)) {
+    mkdir($backupFolder);
 }
-// Backup the database
-$backupFileName = 'backup_' . date('YmdHis') . '.sql';
-$backupFilePath = './' . $backupFileName;
 
-// Execute mysqldump command to create the backup
-exec("mysqldump --user=$username --password=$password --host=$host $database > $backupFilePath");
+$currentYear = date('Y');
+$currentMonth = date('M');
 
-// Close the database connection
-$mysqli->close();
+$yearFolder = $backupFolder . '/' . $currentYear;
+if (!is_dir($yearFolder)) {
+    mkdir($yearFolder);
+}
+
+$monthFolder = $yearFolder . '/' . $currentMonth;
+if (!is_dir($monthFolder)) {
+    mkdir($monthFolder);
+}
+
+$currentDate = date('dMY');
+
+$backupFileName = $currentDate . '_sarmicrosystems_advantage.sql';
+
+$backupFilePath = $monthFolder . '/' . $backupFileName;
+
+$command = "C:\\xampp\\mysql\\bin\\mysqldump.exe --host={$host} --user={$username} --password={$password} {$database} > {$backupFilePath}";
+
+exec($command, $output, $returnValue);
+
+if ($returnValue === 0) {
+    echo "Backup created successfully and saved to {$backupFilePath}";
 
 
-// Provide a link to download the backup file
-if (file_exists($backupFilePath)) {
-    header('Content-Description: File Transfer');
-    header('Content-Type: application/octet-stream');
-    header('Content-Disposition: attachment; filename="' . basename($backupFilePath) . '"');
-    header('Expires: 0');
-    header('Cache-Control: must-revalidate');
-    header('Pragma: public');
-    header('Content-Length: ' . filesize($backupFilePath));
-    readfile($backupFilePath);
-    exit;
+    // Change directory to the root of your Git repository (assuming htdocs is the root)
+    chdir('C:/xampp/htdocs');
+
+    // Git commands
+    $gitAdd = "git add .";
+    $gitCommit = 'git commit -m "auto backup (' . date('Y-m-d') . ')"';
+    $gitPush = "git push -u origin main";
+
+    // Execute Git commands
+    exec($gitAdd, $outputAdd, $returnValueAdd);
+    sleep(60);
+    exec($gitCommit, $outputCommit, $returnValueCommit);
+    sleep(60);
+    exec($gitPush, $outputPush, $returnValuePush);
+
+    // Check for success or failure
+    if ($returnValueAdd === 0) {
+        echo "'git add .' completed successfully.";
+        if ($returnValueCommit === 0) {
+            echo "'git commit' completed successfully.";
+            if ($returnValuePush === 0) {
+                echo "'git push' completed successfully.";
+            } else {
+                echo "Error pushing to Git: " . implode("\n", $outputPush) . " (Return value: {$returnValuePush})";
+            }
+        } else {
+            echo "Error committing to Git: " . implode("\n", $outputCommit) . " (Return value: {$returnValueCommit})";
+        }
+    } else {
+        echo "Error adding changes to Git: " . implode("\n", $outputAdd) . " (Return value: {$returnValueAdd})";
+    }
 } else {
-    echo 'Backup file not found.';
+    echo "Error creating backup: " . implode("\n", $output) . " (Return value: {$returnValue})";
 }
-?>
